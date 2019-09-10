fcf76fbd3ed34259150d6f961937929a67b25b79
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-10 14:02:28 +0700

    Config mongodb for production

--- startup/config.js
+++ startup/config.js
@@ -5,4 +5,13 @@ module.exports = function() {
   if (!config.get("jwtSecretKey")) {
     throw new Error("FATAL ERROR: my_vidly_jwtSecretKey is not set"); // longpkprojwt@1
   }
+  if(process.env.NODE_ENV === "production") {
+    if (!config.get("my_vildy_db_user_name")) {
+      throw new Error("FATAL ERROR: my_vidly_db_user_name is not set"); // longpkprojwt@1
+    }
+    if (!config.get("my_vidly_db_user_password")) {
+      throw new Error("FATAL ERROR: my_vidly_db_user_password is not set"); // longpkprojwt@1
+    }
+  }
+  
 };
--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,13 @@ const config = require("config");
 
 module.exports = function() {
   // Setup connection to MongoDB
-  const db = config.get("db"); 
+  let db = config.get("db");
+  if(process.env.NODE_ENV === "production") {
+    const dbUsername = config.get("dbUsername");
+    const dbUserPassword = config.get("dbUserPassword");
+    db = db.replace("<username>", dbUsername);
+    db = db.replace("<password>", dbUserPassword);
+  }
   mongoose
     .connect(db, {
       useNewUrlParser: true,

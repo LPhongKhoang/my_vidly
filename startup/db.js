aa24dcd1ddd8c62dd14492465c9c6d5d04fc8869
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-15 23:01:13 +0700

    Add default config for db and jwtSecretKey

--- startup/db.js
+++ startup/db.js
@@ -5,12 +5,11 @@ const config = require("config");
 module.exports = function() {
   // Setup connection to MongoDB
   let db = config.get("db");
-  if(process.env.NODE_ENV === "production") {
-    const dbUsername = config.get("dbUsername");
-    const dbUserPassword = config.get("dbUserPassword");
-    db = db.replace("<username>", dbUsername);
-    db = db.replace("<password>", dbUserPassword);
-  }
+  const dbUsername = config.get("dbUsername");
+  const dbUserPassword = config.get("dbUserPassword");
+  db = db.replace("<username>", dbUsername);
+  db = db.replace("<password>", dbUserPassword);
+
   mongoose
     .connect(db, {
       useNewUrlParser: true,

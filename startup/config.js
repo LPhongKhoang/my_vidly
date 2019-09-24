903ec90b03dd62e8c01660c7a339906949ea48d7
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-24 11:05:27 +0700

    adjust config _ only sensitive data in production environment

--- startup/config.js
+++ startup/config.js
@@ -1,11 +1,11 @@
 const config = require("config");
 
 module.exports = function() {
-  // check if jwtSecretKey is set as variable enviroment or not
-  if (!config.get("jwtSecretKey")) {
-    throw new Error("FATAL ERROR: my_vidly_jwtSecretKey is not set"); // longpkprojwt@1
-  }
-  if(process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
+    // check if jwtSecretKey is set as variable enviroment or not
+    if (!config.get("jwtSecretKey")) {
+      throw new Error("FATAL ERROR: my_vidly_jwtSecretKey is not set"); // longpkprojwt@1
+    }
     if (!config.get("dbUsername")) {
       throw new Error("FATAL ERROR: my_vidly_db_user_name is not set"); // longpkprojwt@1
     }
@@ -13,5 +13,4 @@ module.exports = function() {
       throw new Error("FATAL ERROR: my_vidly_db_user_password is not set"); // longpkprojwt@1
     }
   }
-  
 };
--- startup/db.js
+++ startup/db.js
@@ -5,15 +5,18 @@ const config = require("config");
 module.exports = function() {
   // Setup connection to MongoDB
   let db = config.get("db");
-  const dbUsername = config.get("dbUsername");
-  const dbUserPassword = config.get("dbUserPassword");
-  db = db.replace("<username>", dbUsername);
-  db = db.replace("<password>", dbUserPassword);
+  if(process.env.NODE_ENV === "production") {
+    const dbUsername = config.get("dbUsername");
+    const dbUserPassword = config.get("dbUserPassword");
+    db = db.replace("<username>", dbUsername);
+    db = db.replace("<password>", dbUserPassword);
+  } 
 
   mongoose
     .connect(db, {
       useNewUrlParser: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useCreateIndex: true
     })
     .then(() => winston.info(`Connected to ${db}...`));
 };

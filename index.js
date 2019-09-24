2e035a5d24df31dbb819ea1d33f7c95a67108e68
LPhongKhoang <vulongkm1998@gmail.com>
2019-09-24 12:34:37 +0700

    handle CROS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 // Create app server
 const app = express();
 require("./startup/logging")();
+require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/config")();
 require("./startup/db")();
@@ -11,6 +12,6 @@ require("./startup/prod")(app);
 
 
 // get PORT as environment variable or default
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 const server = app.listen(port, () => winston.info(`Server PK pro: started at port ${port}`));
 module.exports = server;
